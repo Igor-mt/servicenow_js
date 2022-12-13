b558c49fde07e0ba4fb49413c358078a725d3ad0
Igor-mt <igor.limall@outlook.com>
2022-12-13 12:07:58 -0300

    Introdução ao JS

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log("Olá, Node.js!");
+nomes = new Array(4);
+nomes[0] = "Vitória";
+nomes[1] = "Igor";
+nomes[2] = "João";
+nomes[3] = "Salvador";
+
+console.log(nomes);
+
+var x = 71;
+
+if(x % 2 == 0){
+    console.log(x, " é um número par!");
+}else {
+    console.log(x, " é um numero impar!");
+}
+
+// Verificação de idade
+
+var idade = 150;
+
+if(idade >= 0 && idade < 12){
+    console.log(idade, " é uma criança.")
+} else if (idade >= 12 && idade < 18) {
+    console.log(idade, " é um Adolecente.")
+} else if (idade >= 18 && idade < 60) {
+    console.log(idade, " é um adulto")
+} else if (idade >= 60 && idade <= 120){
+    console.log(idade, " é um idoso")
+} else {
+    console.log ("Idade inválida!")
+}
