925bdd41ac40ded451787249dca4848dfe1f0b3c
Igor-mt <igor.limall@outlook.com>
2022-12-16 12:44:06 -0300

    Atividade 2 laços de repetições js

---
+++ atividade2_js/script4.js
@@ -0,0 +1,89 @@
+// Supondo que a população de um país A seja da ordem de 80000 habitantes com uma taxa anual de crescimento de 3% e que a população de B seja 200000 habitantes com uma taxa de crescimento de 1.5%. Faça um programa que calcule e escreva o número de anos necessários para que a população do país A ultrapasse ou iguale a população do país B, mantidas as taxas de crescimento.
+
+// var a = 80000;
+// var b = 200000;
+// var ano = 0;
+
+// while (a <=b ){
+//     a += a * 0.03;
+//     b += b * 0.015;
+//     ano += 1
+// }
+
+// alert("A ultrapassa ou iguala a B em " + ano + " anos")
+
+
+// Altere o programa anterior permitindo ao usuário informar as populações e as taxas de crescimento iniciais. 
+
+// var a = parseFloat(prompt("Digite o valor da população da cidade A"));
+// var b = parseFloat(prompt("Digite o valor da população da cidade B"));
+// var ano = 0;
+
+// while (a <= b ){
+//     a += a * 0.03;
+//     b += b * 0.015;
+//     ano += 1
+// }
+
+// alert("A ultrapassa ou iguala a B em " + ano + " anos")
+
+// Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro. 
+
+// var num = 0;
+
+// while(num <= 20){
+//     num++;
+//     document.write(num + "<br>");
+// }
+
+
+// Depois modifique o programa para que ele mostre os números um ao lado do outro.
+
+
+// var num = 0;
+
+// while(num <= 20){
+//     num++;
+//     document.write(num + " ");
+// }
+
+
+// Faça um programa que leia 5 números e informe o maior número.
+// var num1 = parseFloat(prompt("Digite o primeiro numero: "));
+// var num2 = parseFloat(prompt("Digite o primeiro numero: "));
+// var num3 = parseFloat(prompt("Digite o primeiro numero: "));
+// var num4 = parseFloat(prompt("Digite o primeiro numero: "));
+// var num5 = parseFloat(prompt("Digite o primeiro numero: "));
+
+// alert (Math.max(num1,num2,num3,num4,num5))
+
+
+// Faça um programa que leia 5 números e informe a soma e a média dos números. 
+
+// var num1 = parseFloat(prompt("Digite o primeiro numero: "));
+// var num2 = parseFloat(prompt("Digite o primeiro numero: "));
+// var num3 = parseFloat(prompt("Digite o primeiro numero: "));
+// var num4 = parseFloat(prompt("Digite o primeiro numero: "));
+// var num5 = parseFloat(prompt("Digite o primeiro numero: "));
+
+// var soma = num1+num2+num3+num4+num5;
+// var media = soma / 5;
+
+// alert (soma);
+// alert (media);
+
+// Faça um programa que imprima na tela apenas os números ímpares entre 1 e 50.
+
+
+// for (var num1 = 1; num1 <= 50; num1++){
+//     if((num1 % 2) !=0) {
+//         document.write(num1);
+//     }
+// }
+
+
+
+// Faça um programa que receba dois números inteiros e gere os números inteiros que estão no intervalo compreendido por eles.
+
+
+// Altere o programa anterior para mostrar no final a soma dos números.
